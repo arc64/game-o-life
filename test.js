59275c59ca744717ecd5f39f1148fd0e52d1fee6
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-01-16 16:05:18 +0100

    Creating a world for the cells to live and die in.

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 
 var assert = require("assert");
 var newCell = require('./cell');
+var newWorld = require('./world');
 
 describe('cell', function(){
     it('a cell can come to life', function(){
@@ -15,5 +16,21 @@ describe('cell', function(){
       assert.equal(cell.isAlive(), true);
       cell.die();
       assert.equal(cell.isAlive(), false);
+    });
+})
+
+describe('world', function(){
+    it('can generate a world of specific dimentions', function(){
+        var ROWS = 5;
+        var COLUMNS = 4;
+        var world = newWorld().generate(ROWS,COLUMNS,"Awesome");
+        assert.equal(world.length, 4);
+        assert.equal(world[0].length, 5);
+    });
+    it('can generate a world with cells', function(){
+        var cell = newCell(false);
+        var world = newWorld().generate(3,3,cell);
+        assert.equal(typeof(world[0][0]), 'object');
+        assert.equal(world[2][2].isAlive(), false);
     });
 })
---
+++ world.js
@@ -0,0 +1,22 @@
+
+var cell = require('./cell');
+
+var world = function(){
+
+    return {
+        generate: function(height, width, initial) {
+            var matrix = [];
+            for (var i = 0; i < width; i++) {
+                var row = []
+                for (var j = 0; j < height; j++) {
+                    row[j] = initial;
+                }
+                matrix[i] = row;
+            }
+            console.log(matrix)
+            return matrix;
+        }
+    }
+};
+
+module.exports = world;
